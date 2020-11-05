3745b76923ce3623a0b1479351925b222a4e55e0
turpp <white.tripp@icloud.com>
2020-11-05 14:05:15 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Slim Shady"
+    end 
+    get '/hometown' do
+        "My hometown is Da hood"
+    end
+    get '/favorite-song' do
+        "My favorite song is Who let the dogs out"
+    end
+
 end
